7cfd7f27d556fab0b67a3270778556469834b458
huansong <fuhuansong@gmail.com>
2019-04-25 22:17:38 +0000

    ch4:remove signal handling of SIGUSR1 in proress engine
    Right now, just comment it out in MPIDI_Progress_test.

--- src/mpid/ch4/src/ch4_progress.h
+++ src/mpid/ch4/src/ch4_progress.h
@@ -31,15 +31,6 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_Progress_test(int flags)
     MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDI_PROGRESS_TEST);
     MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDI_PROGRESS_TEST);
 
-#ifdef HAVE_SIGNAL
-    if (MPIDI_global.sigusr1_count > MPIDI_global.my_sigusr1_count) {
-        MPIDI_global.my_sigusr1_count = MPIDI_global.sigusr1_count;
-        mpi_errno = MPIDI_check_for_failed_procs();
-        if (mpi_errno)
-            MPIR_ERR_POP(mpi_errno);
-    }
-#endif
-
     if (flags & MPIDI_PROGRESS_HOOKS) {
         MPIDI_PROGRESS_HOOK_TEST(MPIR_NBC_PROGRESS_HOOK_ID, &made_progress);
         MPIDI_PROGRESS_HOOK_TEST(MPII_GENUTIL_PROGRESS_HOOK_ID, &made_progress);
